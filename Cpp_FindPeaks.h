c41dd1df8ee75c1bd6d762a92d024148345da0ce
E24056679-NCKU <E24056679@mail.ncku.edu.tw>
2020-01-06 18:33:47 +0800

    Update Cpp_FindPeaks.h

--- Cpp_FindPeaks.h
+++ Cpp_FindPeaks.h
@@ -3,13 +3,10 @@
 
 #include <vector>
 
-void findLocalMaxima(const std::vector<double> &v, std::vector<double> &maxPks, std::vector<int> &maxLocs);
+void findLocalMaximaLocs(const std::vector<double> &v, std::vector<int> &maxLocs);
 
-void findLocalMinima(const std::vector<double> &v, std::vector<double> &minPks, std::vector<int> &minLocs);
+void findLocalMinimaLocs(const std::vector<double> &v, std::vector<int> &minLocs);
 
-void findLocalExtrema(
-    const std::vector<double> &v,
-    std::vector<double> &maxPks, std::vector<int> &maxLocs,
-    std::vector<double> &minPks, std::vector<int> &minLocs);
+void findLocalExtremaLocs(const std::vector<double> &v, std::vector<int> &maxLocs, std::vector<int> &minLocs);
 
 #endif
